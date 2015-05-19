2cefee261a6d23afdd364117176eb9131502ea3c
Chris Morris <morris.w.chris@gmail.com>
2015-05-18 23:44:40 -0400

    working on site. got card working ... kinda

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -11,6 +11,9 @@ module.exports = {
 		}, {
 			src: "bower_components/bootstrap/dist/js/bootstrap.min.js",
 			dest: "www/assets/js/bootstrap.js"
+		}, {
+			src: "bower_components/stellar/jquery.stellar.min.js",
+			dest: "www/assets/js/stellar.js"
 		}]
 	},
 	src: {
